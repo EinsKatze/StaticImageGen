048fa21271d5495ab52ea2cbc30c7d02c6b4dcbc
EinsKatze <82060534+EinsKatze@users.noreply.github.com>
2023-09-01 10:17:56 +0200

    further speed improvements
    "multithread" the random number generation
    
    and added a lot of comments and i hope this will make it easier to understand this spaghetti in future

--- src/main.go
+++ src/main.go
@@ -8,40 +8,62 @@ import (
 	"image/png"
 	"math/rand"
 	"os"
+	"sync"
 )
 
-func main() {
-	var width int
-	var height int
+var progress int
 
-	flag.IntVar(&width, "w", 64, "Specify width. Default: 64")
-	flag.IntVar(&height, "h", 64, "Specify height. Default: 64")
-	flag.Parse()
-
-	img := image.NewNRGBA(image.Rect(0, 0, width, height))
-	progress := 0
-	maxProg := width * height
-	for y := 0; y < height; y++ {
+func genPixels(start int, end int, width int, img *image.NRGBA, maxProg int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for y := start; y < end; y++ {
 		for x := 0; x < width; x++ {
 			img.Set(x, y, color.RGBA{
 				uint8(rand.Intn(255)),
 				uint8(rand.Intn(255)),
 				uint8(rand.Intn(255)),
 				255})
-			progress += 1
 		}
+		progress += width
 		fmt.Printf("%d/%d pixels generated.\r", progress, maxProg)
 	}
+}
 
-	f, err := os.Create("img.png")
+func main() {
+	var wg sync.WaitGroup
+	var width int
+	var height int
+
+	flag.IntVar(&width, "w", 64, "Specify width. Default: 64")   // Console arg for width
+	flag.IntVar(&height, "h", 64, "Specify height. Default: 64") // Console arg for width
+	flag.Parse()
+	maxProg := width * height // Max Progress
+
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+
+	// Multi-threading the number generation, might reduce speed on old cpus (?), but overall should improve speed.
+	// We could do this on all available threads, but in my case, too many threads reduce the speed instead of increasing it
+	rowsPerWorker := height / 4
+	for i := 0; i < 4; i++ {
+		startY := i * rowsPerWorker
+		endY := startY + rowsPerWorker
+		if i == 3 { // Special case for the last worker to cover any remaining rows
+			endY = height
+		}
+		wg.Add(1)                                            // Increment the wait group to track the number of active workers
+		go genPixels(startY, endY, width, img, maxProg, &wg) // Launch a goroutine to generate pixels for the specified rows
+	}
+
+	f, err := os.Create("img.png") // Create the file, where the img data should be written to
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer f.Close()
 
+	wg.Wait() // Wait for all goroutines to finish before writing content to file
+
 	enc := &png.Encoder{ // Disabling compression in the encoder to speed up encoding
-		CompressionLevel: png.NoCompression, 
+		CompressionLevel: png.NoCompression,
 	}
 	err = enc.Encode(f, img) // Write image data to file
 	if err != nil {
